fa3344464a6d4ec7afd3940513e9357d10cf33af
ThanhDang <xuanthanh.110699@gmail.com>
2022-02-14 14:58:39 +0700

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,8 @@
-//ĐIỂM ĐẠI HỌC
-const benchmarks = document.querySelector("#benchmarks");
-const area = document.querySelector("#area");
-const object = document.querySelector("#object");
-const scores = document.querySelectorAll(".scores");
-const messageScore = document.querySelector(".result-inner");
-const resultSubmit = document.querySelector("#daihoc-button");
-const exciseInner = document.querySelector("#exercises-box");
+// MENU CLICK ACTIVE AND READMORE CLICK
 const menuExercises = document.querySelectorAll(".exercise");
 const readMores = document.querySelectorAll(".summary-title");
 const readMorecontents = document.querySelectorAll(".summary-list");
-
-readMores.forEach((e, index) => {
-  e.addEventListener("click", () => {
-    for (let i = 0; i < readMores.length; i++) {
-      if (index == i) {
-        readMorecontents[i].classList.toggle("open");
-      }
-    }
-  });
-});
-
-function innerResult() {
-  let benchmark = Number(benchmarks.value);
-  let areaPoint = Number(area.value);
-  let objectPoint = Number(object.value);
-  var totalScores = 0;
-  for (i = 0; i < scores.length; i++) {
-    if (Number(scores[i].value) == 0) {
-      totalScores = "";
-    } else {
-      totalScores += Number(scores[i].value);
-    }
-  }
-  if (typeof totalScores !== "number") {
-    messageScore.innerText = "Bạn đã rớt, do có ít nhất 1 điểm 0";
-  } else {
-    totalScores = (totalScores + areaPoint + objectPoint).toFixed(3);
-    if (totalScores < benchmark) {
-      messageScore.innerText = "Bạn đã RỚT. Tổng điểm :" + " " + totalScores;
-    } else {
-      messageScore.innerText = "Bạn đã ĐẬU. Tổng điểm :" + " " + totalScores;
-    }
-  }
-}
+const exerciseNumbers = document.querySelectorAll(".exercises-content");
 
 function numberActive() {
   for (let menuExercise of menuExercises) {
@@ -51,68 +11,6 @@ function numberActive() {
   this.classList.add("active");
 }
 
-resultSubmit.addEventListener("click", innerResult);
-
-//TIỀN CÁP
-
-const userType = document.querySelector("#type-user");
-const userId = document.querySelector("#user-id");
-const userChannel = document.querySelector(".channels");
-const userConnect = document.querySelector(".connect");
-const tiencapButton = document.querySelector("#tiencap-button");
-const resultTienCap = document.querySelector("#result-tiencap");
-const popularFee = 4.5;
-const businessFee = 15;
-const popularChannel = 7.5;
-const businessChannel = 50;
-const popularBasic = 20.5;
-const first10Connect = 75;
-const businessConnect = 5;
-
-function userTypeChange() {
-  if (userType.value == 2) {
-    userConnect.classList.add("open");
-  } else {
-    userConnect.classList.remove("open");
-  }
-}
-
-function tinhtienCap() {
-  let tongTien = 0;
-  if (userType.value == 0) {
-    alert("Chọn loại khách hàng");
-  } else {
-    if (userType.value == 1) {
-      tongTien =
-        Number(userChannel.value) * popularChannel + popularBasic + popularFee;
-    } else if (userType.value == 2) {
-      if (userConnect.value <= 10) {
-        tongTien =
-          Number(userChannel.value) * businessChannel +
-          businessFee +
-          first10Connect;
-      } else {
-        tongTien =
-          Number(userChannel.value) * businessChannel +
-          businessFee +
-          (Number(userConnect.value) - 10) * businessConnect +
-          first10Connect;
-      }
-    }
-    console.log(tongTien);
-    resultTienCap.innerText =
-      "Mã khác hàng:   " +
-      userId.value +
-      "; " +
-      "Tiền cáp: " +
-      (tongTien.toFixed(2).toLocaleString() + "$");
-  }
-}
-
-userType.addEventListener("change", userTypeChange);
-tiencapButton.addEventListener("click", tinhtienCap);
-
-const exerciseNumbers = document.querySelectorAll(".exercises-content");
 menuExercises.forEach((e, index) => {
   e.addEventListener("click", numberActive);
   e.addEventListener("click", () => {
@@ -126,107 +24,310 @@ menuExercises.forEach((e, index) => {
   });
 });
 
-// TIỀN THUẾ
-
-const userIncome = document.querySelector(".income");
-const userName = document.querySelector("#user-name");
-const dependentPerson = document.querySelector(".people");
-const tienthueButton = document.querySelector("#tienthue-button");
-const resultTienThue = document.querySelector("#result-tienthue");
-const dependentCost = 1.6e6;
-const moneyOffset = 4e6;
-
-function tienThue() {
-  let tongThunhap = Number(userIncome.value);
-  let depenPerson = Number(dependentPerson.value);
-  let chiuThue = tongThunhap - moneyOffset - depenPerson * dependentCost;
-  if (chiuThue <= 0) {
-    alert("Thu nhập không hợp lệ");
-  } else {
-    let tienThue = 0;
-    if (chiuThue <= 60e6) {
-      tienThue = chiuThue * 0.05;
-    } else if (chiuThue > 60e6 && chiuThue <= 120e6) {
-      tienThue = chiuThue * 0.1;
-    } else if (chiuThue > 120e6 && chiuThue <= 210e6) {
-      tienThue = chiuThue * 0.15;
-    } else if (chiuThue > 210e6 && chiuThue <= 384e6) {
-      tienThue = chiuThue * 0.2;
-    } else if (chiuThue > 384e6 && chiuThue <= 624e6) {
-      tienThue = chiuThue * 0.25;
-    } else if (chiuThue > 624e6 && chiuThue <= 960e6) {
-      tienThue = chiuThue * 0.3;
-    } else if (chiuThue > 960e6) {
-      tienThue = chiuThue * 0.35;
+readMores.forEach((e, index) => {
+  e.addEventListener("click", () => {
+    for (let i = 0; i < readMores.length; i++) {
+      if (index == i) {
+        readMorecontents[i].classList.toggle("open");
+      }
     }
-    tienThue = tienThue.toLocaleString() + " VNĐ";
-    resultTienThue.innerText =
-      "Họ tên:  " +
-      userName.value +
-      ";  " +
-      "Tiền thuế thu nhập cá nhân:  " +
-      tienThue;
-  }
-}
-
-tienthueButton.addEventListener("click", tienThue);
-
-//TIỀN ĐIỀN
-
-const userNameElectric = document.querySelector("#user-name2");
-const electricNumber = document.querySelector(".electric-number");
-const electricButton = document.querySelector("#tiendien-button");
-const resultTienDien = document.querySelector("#result-tiendien");
-const first50Kwh = 500;
-const second50Kwh = 650;
-const third50Kwh = 850;
-const fourth50Kwh = 1100;
-const remainKwh = 1300;
-const first50KwhCost = first50Kwh * 50;
-const second50KwhCost = second50Kwh * 50;
-const third50KwhCost = third50Kwh * 100;
-const fourth50KwhCost = fourth50Kwh * 150;
-
-function electricBill() {
-  let electricCount = Number(electricNumber.value);
-  let electricMoney = 0;
-  if (electricCount <= 0) {
-    alert("Nhập đúng Kw điện!");
-  } else {
-    if (electricCount <= 50) {
-      electricMoney = electricCount * first50Kwh;
-    } else if (electricCount > 50 && electricCount <= 100) {
-      electricMoney = first50KwhCost + (electricCount - 50) * second50Kwh;
-    } else if (electricCount > 100 && electricCount <= 200) {
-      electricMoney =
-        first50KwhCost + second50KwhCost + (electricCount - 100) * third50Kwh;
-    } else if (electricCount > 200 && electricCount <= 350) {
-      electricMoney =
-        first50KwhCost +
-        second50KwhCost +
-        third50KwhCost +
-        (electricCount - 200) * fourth50Kwh;
-    } else {
-      electricMoney =
-        first50KwhCost +
-        second50KwhCost +
-        third50KwhCost +
-        fourth50KwhCost +
-        (electricCount - 350) * remainKwh;
-    }
-    electricMoney = electricMoney.toLocaleString() + " VNĐ";
-    resultTienDien.innerText =
-      "Họ và tên:  " +
-      userNameElectric.value +
-      ";   " +
-      "Tiền điện:   " +
-      electricMoney;
-  }
-}
-
-electricButton.addEventListener("click", electricBill);
-electricNumber.addEventListener("keyup", (e) => {
-  if (e.key == 13) {
-    electricBill;
-  }
+  });
 });
+
+// PUSH ARRAY
+
+const userInput = document.querySelector("#input-array");
+const arrayButton = document.querySelector(".array-push");
+const arrayInner = document.querySelector("#array-print");
+
+var arrayInput = [];
+var arrayLenght = arrayInput.length;
+
+function pushNumber(input, array, inner) {
+  let numberInput = Number(input.value);
+  array.push(numberInput);
+  inner.innerText = "Mảng đã nhập:   " + "[" + array.join(", ") + "]";
+  arrayLenght = array.length;
+  console.log(arrayInput);
+}
+
+arrayButton.addEventListener("click", () => {
+  pushNumber(userInput, arrayInput, arrayInner);
+});
+
+// DELETE ARRAY
+
+const deleteButton = document.querySelector(".array-delete");
+const deleteNumbutton = document.querySelector(".number-delete");
+
+function deleteNumber(array, inner) {
+  if (arrayLenght) {
+    array.pop();
+    arrayInput = array;
+    arrayLenght = array.length;
+    inner.innerText = "Mảng đã nhập:   " + "[" + array.join(", ") + "]";
+    console.log(arrayLenght);
+  } else {
+    inner.innerText = '"Nhập các phần tử của mảng ở trên"';
+  }
+}
+
+function deleteArrray() {
+  if (arrayLenght) {
+    arrayInput = [];
+    arrayInner.innerText = '"Nhập các phần tử của mảng ở trên"';
+    for (let arr of document.querySelectorAll(".result")) {
+      arr.innerText = "Nhập dữ liệu ở trên";
+    }
+  }
+}
+
+deleteButton.addEventListener("click", deleteArrray);
+deleteNumbutton.addEventListener("click", () => {
+  deleteNumber(arrayInput, arrayInner);
+});
+
+// TỔNG SỐ DƯƠNG
+
+const innerPosnumber = document.querySelector(".result-posnumber");
+const posnumberButton = document.querySelector("#posnumber-button");
+
+function posNumbers(array) {
+  let totalNumber = 0;
+  for (let i = 0; i < arrayLenght; i++) {
+    if (array[i] > 0) {
+      totalNumber += array[i];
+    }
+  }
+  innerPosnumber.innerText = "Tổng các số dương:   " + totalNumber;
+}
+
+posnumberButton.addEventListener("click", () => {
+  posNumbers(arrayInput);
+  alertInput(arrayInput, innerPosnumber);
+});
+
+//ĐẾM SỐ DƯƠNG
+
+const countNumberpos = document.querySelector(".count-posnumber");
+const countPosbutton = document.querySelector("#count-posnumber-button");
+
+function countPosnumber(array) {
+  let count = 0;
+  for (let i = 0; i < arrayLenght; i++) {
+    if (array[i] > 0) {
+      count++;
+    }
+  }
+  countNumberpos.innerText = "Tổng số dương:   " + count + " số";
+}
+
+countPosbutton.onclick = () => {
+  countPosnumber(arrayInput);
+  alertInput(arrayInput, countNumberpos);
+};
+
+//TÌM SỐ NHỎ NHẤT
+
+const minNumberbutton = document.querySelector("#minnumber-button");
+const minNumber = document.querySelector(".minnumber");
+
+function minNumberfun(array) {
+  minNumber.innerText = "Số nhỏ nhất trong mảng:  " + Math.min(...array);
+}
+
+minNumberbutton.addEventListener("click", () => {
+  minNumberfun(arrayInput);
+  alertInput(arrayInput, minNumber);
+});
+
+//TÌM SỐ DƯƠNG NHỎ NHẤT
+
+const minPosbutton = document.querySelector("#minpos-button");
+const minPos = document.querySelector(".minpos-number");
+
+function minNumberpos(array) {
+  let posArray = [];
+  for (let arr of array) {
+    if (arr > 0) {
+      posArray.push(arr);
+    }
+  }
+  if (!posArray.length) {
+    minPos.innerText = "Số dương nhỏ nhất trong mảng:  " + "Không có số dương";
+  } else {
+    minPos.innerText =
+      "Số dương nhỏ nhất trong mảng:  " + Math.min(...posArray);
+  }
+}
+
+minPosbutton.addEventListener("click", () => {
+  minNumberpos(arrayInput);
+  alertInput(arrayInput, minPos);
+});
+
+// SỐ CHẲN CUỐI CÙNG
+
+const finalNumberbutton = document.querySelector("#final-button");
+const finalNumber = document.querySelector(".final-number");
+
+function finalEvenumber(array) {
+  let finalNum = 0;
+  let count = 0;
+  for (let arr of array) {
+    if (arr % 2 == 0 && arr != 0 && arrayLenght != 0) {
+      finalNum = arr;
+      count++;
+    }
+    if (count == 0) {
+      finalNum = "Không có số chẵn";
+    }
+  }
+
+  finalNumber.innerText = "Số chẵn cuối cùng trong mảng:  " + finalNum;
+}
+
+finalNumberbutton.addEventListener("click", () => {
+  finalEvenumber(arrayInput);
+  alertInput(arrayInput, finalNumber);
+});
+
+//ĐỔI CHỖ
+
+const convertNumbutton = document.querySelector("#convert-button");
+const convertNum = document.querySelector(".convert-number");
+const indexNum1 = document.querySelector(".index-number1");
+const indexNum2 = document.querySelector(".index-number2");
+
+function convertNumber(array, index1, index2) {
+  for (let arr of array) {
+    let temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+  }
+  convertNum.innerText = "Mảng sau khi đổi:  " + "[" + array.join(", ") + "]";
+}
+
+convertNumbutton.addEventListener("click", () => {
+  let index1 = Number(indexNum1.value);
+  let index2 = Number(indexNum2.value);
+  convertNumber(arrayInput, index1, index2);
+  alertInput(arrayInput, convertNum);
+});
+
+// SẮP XẾP
+
+const sortNumberbutton = document.querySelector("#sort-button");
+const sortNum = document.querySelector(".sort-number");
+
+function sortNumber(array) {
+  for (let i = 0; i < arrayLenght; i++) {
+    for (let j = 0; j < arrayLenght; j++) {
+      if (array[i] < array[j]) {
+        let temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+  sortNum.innerText = "Mảng đã sắp xếp:  " + "[" + array.join(", ") + "]";
+}
+
+sortNumberbutton.addEventListener("click", () => {
+  sortNumber(arrayInput);
+  alertInput(arrayInput, sortNum);
+});
+
+//TÌM SỐ NGUYÊN TỐ
+
+const primeNumberbutton = document.querySelector("#prime-button");
+const primeNum = document.querySelector(".prime-number");
+
+function primeNumber(array) {
+  let prime = 0;
+  for (let arr of array) {
+    if (arr >= 2) {
+      let count = 0;
+      for (let i = 2; i <= Math.sqrt(arr); i++) {
+        if (arr % i == 0) {
+          count++;
+        }
+      }
+      if (count == 0) {
+        prime = arr;
+        break;
+      }
+    }
+  }
+  primeNum.innerText = "Số nguyên tố đầu tiên:  " + prime;
+}
+
+primeNumberbutton.addEventListener("click", () => {
+  primeNumber(arrayInput);
+  alertInput(arrayInput, primeNum);
+});
+
+// ĐẾM SỐ NGUYÊN
+const integerInput = document.querySelector("#input-array-integer");
+const arrayIntebutton = document.querySelector(".array-integer-push");
+const arrInner = document.querySelector("#array-integer-print");
+const integerButton = document.querySelector("#integer-button");
+const integerNum = document.querySelector(".integer-number");
+
+var arrInteger = [];
+
+arrayIntebutton.addEventListener("click", () => {
+  pushNumber(integerInput, arrInteger, arrInner);
+});
+
+function integerCount(array) {
+  let count = 0;
+  for (let arr of array) {
+    if (Number.isInteger(arr)) {
+      count++;
+    }
+  }
+  integerNum.innerText = "Số nguyên:   " + count + "  số";
+}
+
+integerButton.addEventListener("click", () => {
+  integerCount(arrInteger);
+  alertInput(arrInteger, integerNum);
+});
+
+// SO SÁNH
+
+const compareButton = document.querySelector("#compare-button");
+const compareNum = document.querySelector(".compare-number");
+
+function compareNumber(array, inner) {
+  let countNeg = 0;
+  let countPos = 0;
+  for (let arr of array) {
+    if (arr > 0) {
+      countPos++;
+    } else if (arr < 0) {
+      countNeg++;
+    }
+  }
+  if (countNeg > countPos) {
+    inner.innerText = "Số dương" + " < " + "Số âm";
+  } else if (countNeg < countPos) {
+    inner.innerText = "Số dương" + " > " + "Số âm";
+  } else {
+    inner.innerText = "Số dương" + " = " + "Số âm";
+  }
+}
+
+compareButton.addEventListener("click", () => {
+  compareNumber(arrayInput, compareNum);
+  alertInput(arrayInput, compareNum);
+});
+
+// CẢNH BÁO CHƯA NHẬP LIỆU
+
+function alertInput(array, inner) {
+  if (array.length == 0) {
+    inner.innerText = "Vui lòng nhập dữ liệu ở trên";
+  }
+}
